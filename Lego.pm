2b74205104189ff6218cdd31c24b04f539f72709
Michal Špaček <tupinek@gmail.com>
2015-03-31 22:26:57 +0200

    First stub.

---
+++ Lego.pm
@@ -0,0 +1,50 @@
+package Task::Lego;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Mock::Person - Install the Mock::Person modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Mock::Person
+
+=head1 SEE ALSO
+
+L<Lego::Part>,
+L<Lego::Part::Image>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Task-Lego>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
