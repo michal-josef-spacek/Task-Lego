a65d08e9c57dfe261b6b57919e0a72166358bed1
Michal Josef Špaček <michal.spacek@gooddata.com>
2017-12-13 19:44:03 +0100

    Update author name

--- Lego.pm
+++ Lego.pm
@@ -63,13 +63,13 @@ L<https://github.com/tupinek/Task-Lego>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2015 Michal Špaček
+ © 2015 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
